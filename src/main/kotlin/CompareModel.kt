1f5d492bc6d66b3b2beaa2f058392cb99eef124f
Richard Liebig <edis.osterhage@unibw.de>
2020-10-11 12:39:26 +0200

    Created Compare Model utility

--- src/main/kotlin/CompareModel.kt
+++ src/main/kotlin/CompareModel.kt
@@ -1,5 +1,29 @@
-
+import models.createKeywordString
 
 fun main() {
+    val firstCampaignName = "2020-10-11T12-18-36.975803München-Softwareentwickler-4"
+    val secondCampaignName = "2020-10-10T12-29-44.978996München-Softwareentwickler-4"
 
+    val firstModel = ModelInstance()
+    val secondModel = ModelInstance()
+
+    firstModel.readModel("$firstCampaignName/model.txt")
+    secondModel.readModel("$secondCampaignName/model.txt")
+
+    val hashMap = HashMap<String, Int>()
+
+    //Here comes an excruating slow operation
+    for (key in firstModel.items.keys) {
+         if(secondModel.items.keys.contains(key)) {
+             //It seems like this code needs to be further improved in this regard.
+             val diff = firstModel.items[key]?.minus(secondModel.items[key]!!)
+             hashMap[key] = diff!!
+         } else {
+             hashMap[key] = firstModel.items[key]!!
+         }
+    }
+
+    for(key in hashMap.keys) {
+        println("$key: ${hashMap[key]}")
+    }
 }
--- src/main/kotlin/Model.kt
+++ src/main/kotlin/Model.kt
@@ -5,89 +5,174 @@ import java.io.File
 import java.io.FileReader
 import java.io.FileWriter
 
-class ModelInstance {
+
+
+object Model {
     var items  = HashMap<String,Int>()
-                                                        object Model {
-    fun filter() {    var items  = HashMap<String,Int>()
+
+    fun filter() {
         val newItems = HashMap<String, Int>()
-        for (item in items) {    fun filter() {
-            if(!bansWord.contains(item.key)) {        val newItems = HashMap<String, Int>()
-                newItems[item.key] = item.value        for (item in items) {
-            }            if(!bansWord.contains(item.key)) {
-        }                newItems[item.key] = item.value
-                                                                    }
-        items = newItems        }
+        for (item in items) {
+            if(!bansWord.contains(item.key)) {
+                newItems[item.key] = item.value
+            }
+        }
+
+        items = newItems
     }
-                                                                items = newItems
-    fun saveModel() {    }
+
+    fun saveModel() {
         val writer = FileWriter(Configuration.modelFile)
-        items.forEach {    fun saveModel() {
-            (t,u) ->        val writer = FileWriter(Configuration.modelFile)
-                writer.write("$t=$u\n")        items.forEach {
-        }            (t,u) ->
-        writer.close()                writer.write("$t=$u\n")
-    }        }
-                                                                writer.close()
-    fun readModel() {    }
+        items.forEach {
+            (t,u) ->
+                writer.write("$t=$u\n")
+        }
+        writer.close()
+    }
+
+    fun readModel() {
         val newItems = HashMap<String, Int>()
-                                                            fun readModel() {
-        val reader = FileReader(Configuration.modelFile)        val newItems = HashMap<String, Int>()
+
+        val reader = FileReader(Configuration.modelFile)
         reader.forEachLine {
-            line ->        val reader = FileReader(Configuration.modelFile)
-            println(line)        reader.forEachLine {
-            try {            line ->
-                val key = line.split("=")[0]            println(line)
-                val value = line.split("=")[1]            try {
-                newItems[key] = value.toInt()                val key = line.split("=")[0]
-            } catch (e : Exception) {                val value = line.split("=")[1]
-                println(e.toString())                newItems[key] = value.toInt()
-            }            } catch (e : Exception) {
-        }                println(e.toString())
-        reader.close()            }
-        items = newItems        }
-    }        reader.close()
-                                                                items = newItems
-                                                            }
+            line ->
+            println(line)
+            try {
+                val key = line.split("=")[0]
+                val value = line.split("=")[1]
+                newItems[key] = value.toInt()
+            } catch (e : Exception) {
+                println(e.toString())
+            }
+        }
+        reader.close()
+        items = newItems
+    }
+
+
     val bansWord = listOf(
         "den",
-        "der",    val bansWord = listOf(
-        "an",        "den",
-        "ein",        "der",
-        "die",        "an",
-        "bei",        "ein",
-        "ist",        "die",
-        "du",        "bei",
-        "bzw.",        "ist",
-        "im",        "du",
-        "in",        "bzw.",
-        "",        "im",
-        "z.b.",        "in",
-        "gerne",        "",
-        "sowie",        "z.b.",
-        "und",        "gerne",
-        "eine",        "sowie",
-        "kenntnisse",        "und",
-        "oder",        "eine",
-        "hast",        "kenntnisse",
-        "dein",        "oder",
-        "von",        "hast",
-        "sie",        "dein",
-        "vorzugsweise",        "von",
-        "bspw.",        "sie",
-            "dich",        "vorzugsweise",
-        "zur",        "bspw.",
-        "mit",            "dich",
-        "beim",        "zur",
-        "zu",        "mit",
-        "über",        "beim",
-        "wie",        "zu",
-        "haben",        "über",
-        "-",        "wie",
-        "kentnisse",        "haben",
-        "zum",        "-",
-        "and",        "kentnisse",
-        "auf"        "zum",
-    )        "and",
-}        "auf"
+        "der",
+        "an",
+        "ein",
+        "die",
+        "bei",
+        "ist",
+        "du",
+        "bzw.",
+        "im",
+        "in",
+        "",
+        "z.b.",
+        "gerne",
+        "sowie",
+        "und",
+        "eine",
+        "kenntnisse",
+        "oder",
+        "hast",
+        "dein",
+        "von",
+        "sie",
+        "vorzugsweise",
+        "bspw.",
+            "dich",
+        "zur",
+        "mit",
+        "beim",
+        "zu",
+        "über",
+        "wie",
+        "haben",
+        "-",
+        "kentnisse",
+        "zum",
+        "and",
+        "auf"
+    )
+}
+
+class ModelInstance {
+    var items  = HashMap<String,Int>()
+
+    fun filter() {
+        val newItems = HashMap<String, Int>()
+        for (item in items) {
+            if(!bansWord.contains(item.key)) {
+                newItems[item.key] = item.value
+            }
+        }
+
+        items = newItems
+    }
+
+    fun saveModel() {
+        val writer = FileWriter(Configuration.modelFile)
+        items.forEach {
+            (t,u) ->
+                writer.write("$t=$u\n")
+        }
+        writer.close()
+    }
+
+    fun readModel(fileName : String) {
+        val newItems = HashMap<String, Int>()
+
+        val reader = FileReader(fileName)
+        reader.forEachLine {
+            line ->
+            println(line)
+            try {
+                val key = line.split("=")[0]
+                val value = line.split("=")[1]
+                newItems[key] = value.toInt()
+            } catch (e : Exception) {
+                println(e.toString())
+            }
+        }
+        reader.close()
+        items = newItems
+    }
+
+
+    val bansWord = listOf(
+        "den",
+        "der",
+        "an",
+        "ein",
+        "die",
+        "bei",
+        "ist",
+        "du",
+        "bzw.",
+        "im",
+        "in",
+        "",
+        "z.b.",
+        "gerne",
+        "sowie",
+        "und",
+        "eine",
+        "kenntnisse",
+        "oder",
+        "hast",
+        "dein",
+        "von",
+        "sie",
+        "vorzugsweise",
+        "bspw.",
+            "dich",
+        "zur",
+        "mit",
+        "beim",
+        "zu",
+        "über",
+        "wie",
+        "haben",
+        "-",
+        "kentnisse",
+        "zum",
+        "and",
+        "auf"
     )
 }
